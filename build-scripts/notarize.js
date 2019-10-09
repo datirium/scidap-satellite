c4f6dde8e0a66e4e075408d9d01573f6c45b51a0
Andrey Kartashov <porter@porter.st>
2019-10-09 14:56:46 -0400

    notarization

---
+++ build-scripts/notarize.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+const password = `@keychain:AC_SS_PASSWORD`;
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.datirium.scidap-satellite',
+    appPath: `${appOutDir}/${appName}.app`,
+    // appleId: process.env.APPLEID,
+    // appleIdPassword: process.env.APPLEIDPASS,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: password,
+    ascProvider: 'DATIRIUM LLC',
+  });
+};
