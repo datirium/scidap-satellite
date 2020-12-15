1ea1bf2a0c31ade0b493975dd8192b7389473877
heylel-b-sh <heylel.b.sh@gmail.com>
2020-12-14 19:43:46 -0500

    Switch Airflow to PostgreSQL in ubuntu bundle

---
+++ build-scripts/ubuntu/scripts/start_scheduler.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export PGHOST="${PGHOST:="127.0.0.1"}"
+export PGPORT="${PGPORT:="5432"}"
+export PGDATA="${PGDATA:="~/pgdata"}"
+export PGUSER="${PGUSER:="pg_user"}"
+export PGPASSWORD="${PGPASSWORD:="pg_password"}"
+export PGDATABASE="${PGDATABASE:="pg_database"}"
+export AIRFLOW_HOME="${AIRFLOW_HOME:="~/airflow"}"
+export PROCESS_REPORT_URL="${PROCESS_REPORT_URL:="http://127.0.0.1:3069"}"
+
+echo "Wait until required database is ready"
+until pg_isready --dbname="${PGDATABASE}"
+do
+    echo "Sleep 1 sec"
+    sleep 1;
+done
+
+echo "Run initial configuration for CWL-Airflow"
+cwl-airflow init --upgrade
+
+echo "Recreate process_report connection"
+airflow connections -d --conn_id process_report
+airflow connections -a --conn_id process_report --conn_uri "${PROCESS_REPORT_URL}"
+
+echo "Start airflow scheduler"
+airflow scheduler "$@"
