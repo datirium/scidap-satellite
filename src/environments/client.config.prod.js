3f11380d5963612bda433302c28a6890ff25d165
heylel-b-sh <heylel.b.sh@gmail.com>
2021-02-22 16:37:06 -0500

    Split electron:mac into dev and prod subcommands

---
+++ src/environments/client.config.prod.js
@@ -0,0 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+__meteor_runtime_config__ = {
+    "meteorEnv": {},
+    "DDP_DEFAULT_CONNECTION_URL": "https://api.scidap.com/",
+    "ROOT_URL": "https://scidap.com/",
+    "autoupdate": {
+      "versions": {}
+    }
+};
