19e1ca00d27959d7e965aa4abbea6166b9c6909a
heylel-b-sh <heylel.b.sh@gmail.com>
2021-04-06 12:06:24 -0400

    Update node to the latest 12.22.0

--- build-scripts/build_macos.sh
+++ build-scripts/build_macos.sh
@@ -37,7 +37,7 @@ build_njs_client() {
 
 
 # Setting up package versions
-NODE_VERSION="12.20.2"
+NODE_VERSION="12.22.0"
 ARIA2_VERSION="1.35.0"
 CWLAIRFLOW_VERSION="1.2.10"
 CWLAIRFLOW_PYTHON_VERSION="3.8"
