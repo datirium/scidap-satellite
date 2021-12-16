ae6a34ca90a017286e207865c90ba77beeaf0590
Daniel Troyano <daniel.troyano@datirium.com>
2021-12-16 14:20:11 -0600

    Older changes

---
+++ build-scripts/start_scripts/start_webserver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export PGHOST="${PGHOST:="127.0.0.1"}"
+export PGPORT="${PGPORT:="5432"}"
+export PGDATA="${PGDATA:="~/pgdata"}"
+export PGUSER="${PGUSER:="pg_user"}"
+export PGPASSWORD="${PGPASSWORD:="pg_password"}"
+export PGDATABASE="${PGDATABASE:="pg_database"}"
+
+echo "Wait until required database is ready"
+until pg_isready --dbname="${PGDATABASE}"
+do
+    echo "Sleep 1 sec"
+    sleep 1;
+done
+
+
+echo "Start airflow webserver"
+airflow webserver
+
--- build-scripts/utilities/configure.js
+++ build-scripts/utilities/configure.js
@@ -245,7 +245,7 @@ function getSettings(cwd, customLocation){
   const settings_locations = [
     process.env.SCIDAP_SETTINGS,
     path.resolve(cwd, '../../scidap_settings.json'),
-    path.resolve(os.homedir(), './.config/scidap-satellite/scidap_settings.json'),  // might be different on mac
+    path.resolve(os.homedir(), './.config/scidap_satellite/scidap_settings.json'),  // might be different on mac
     path.resolve(cwd, '../configs/scidap_default_settings.json')                    // default settings should be always present
   ];
 
@@ -262,6 +262,7 @@ function getSettings(cwd, customLocation){
       startPostgres: path.resolve(cwd, '../satellite/bin/start_postgres.sh'),
       startScheduler: path.resolve(cwd, '../satellite/bin/start_scheduler.sh'),
       startApiserver: path.resolve(cwd, '../satellite/bin/start_apiserver.sh'),
+      startWebserver: path.resolve(cwd, '../satellite/bin/start_webserver.sh'),
       startNjsClient: path.resolve(cwd, '../satellite/dist/src/main.js'),
       satelliteBin: path.resolve(cwd, '../satellite/bin'),                       // not used directly, but added just in case
       cwlAirflowBin: path.resolve(cwd, '../cwl-airflow/bin_portable'),
@@ -328,7 +329,20 @@ function getRunConfiguration(settings){
         exec_mode: 'fork_mode',
         cwd: settings.defaultLocations.airflow,
         env: getNjsClientEnvVar(settings)
-      }
+      },
+      {
+        name: 'airflow-webserver',
+        script: settings.executables.startWebserver,
+        args: [],
+        interpreter: 'bash',
+        watch: false,
+        exec_mode: 'fork_mode',
+        cwd: settings.defaultLocations.airflow,
+        env: {
+          ...getPostgresEnvVar(settings),
+          ...getAirflowEnvVar(settings)
+        }
+      },
     ]
   };
 
